e9f83f576dafa06058fceb3e5c3aa08e8248a124
IMACULGY <drewisaacson@live.com>
2020-12-22 10:54:04 -0500

    Fixed app for heroku

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const handler = require('./handler');
 
-//Keys located in key.pem and cert.pem
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
 
 //Create the HTTPS Server
 const port = process.env.PORT || 8080
-https.createServer(options, function (req, res) {
+http.createServer(function (req, res) {
     console.log(req.method);
     res = handler.handleRequest(req, res);
     //console.log(res);
